63e90ae42fe0dc6768f959c1895683e43db52eb6
java80 <iekedede@hotmail.com>
2021-05-19 14:47:49 -0400

    Header component done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
 
+import "./Header.css"
 const Header = () => {
   return (
-    <div>
-      <div>
-      <img src={process.env.PUBLIC_URL + '/img/linkedIn-icon.png'} alt="LinkInProfile" />
-      </div>
-      <div>
+    <>
+    <div className="header-container">
+      <div className = 'job-title'>
       <h1>Ikechukwu Ekedede</h1>
+        <h2>Software Engineer</h2>
       </div>
-      <div>
-      <h1>Software Engineer</h1>
+      <div className = "linkin-profile">
+      <img src={process.env.PUBLIC_URL + '/img/linkedin.jpg'} alt="LinkInProfile" />
+       
       </div>
-      <div>
+      {/* <div className = "social-linkin">
         <img src={process.env.PUBLIC_URL + '/img/linkedIn-icon.png'} alt="LinkInProfile" />
-        <img src= {process.env.PUBLIC_URL + '/img/github-icon.png'} alt ="GitHubProfile"/>
+        
       </div>
+      <div className = "social-github">
+       <img src= {process.env.PUBLIC_URL + '/img/github-icon.png'} alt ="GitHubProfile"/>
+      </div> */}
     
-    </div>
+      </div>
+      <div className = "social-container">
+      <div className = "social-github">
+       <img src= {process.env.PUBLIC_URL + '/img/github-icon.png'} alt ="GitHubProfile"/>
+      </div>
+      <div className = "social-linkin">
+        <img src={process.env.PUBLIC_URL + '/img/linkedIn-icon.png'} alt="LinkInProfile" />
+        
+        </div>
+        </div>
+      </>
   );
 };
 
