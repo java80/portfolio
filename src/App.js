3d59da2e018e3ad5d20c9c9ba4f7a9897f2f8f71
java80 <iekedede@hotmail.com>
2021-05-20 21:23:04 -0400

    About  me component done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Aboutme from "./components/Aboutme/Aboutme";
+import './App.css';
+import Projects from "./components/Projects/Projects";
 
-import Header from "./components/Header/Header"
+
 function App() {
+  const headings = {
+    language: "Languages",
+     os: "OS",
+    framework: "Frameworks",
+    tools: "Tools/SDKs"
+  };
   return (
     <div className="App">
+      
       <Header />
+      <div className = "main-container">
+        <Aboutme headings={headings} />
+        <Projects/>
+      </div>
+      <Footer />
+      
     </div>
   );
 }
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Projects.css";
+const Projects = () => {
+  return (
+    <div className = "project-card">
+      <h1> Recent Projects</h1>
+      jldsajldfsjldfs
+      sdafjfdjsl;
+    </div>
+  );
+};
+
+export default Projects;
