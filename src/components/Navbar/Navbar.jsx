58c3675a79bc452ed5ac73729171e18cc5c11c61
java80 <iekedede@hotmail.com>
2021-06-27 15:08:59 -0400

    Navbar markup done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
-
+import { Person,Mail } from '@material-ui/icons'
 const Navbar = () => {
   return (
-    <div>
-      <h1>Hello Navbar</h1>
+    <div className="top-bar">
+      <div className="left">
+        <div className="wrapper">
+          Ike's Portfolio
+          <div className="item-container">
+            <Person className="icon" />
+            <span>8626001706</span>
+          </div>
+          <div className="item-container">
+            <Mail className="mail" />
+
+            <span>ikeekedede@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
