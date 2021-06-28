4836b148635dabcac1989468bf407a38b1eb3502
java80 <iekedede@hotmail.com>
2021-06-28 15:47:42 -0400

    Installed npm i ityped

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {useRef} from 'react'
+ 
 
 const Intro = () => {
+  const textRef = useRef();
   return (
-    <div>
-      <h1>Hello Intro</h1>
+    <div className="intro" id="intro">
+      <div className="wraper">
+        <h2> Hi There, I'm</h2>
+        <h1>Ikechukwu Ekedede</h1>
+        <h3>Freelance <span ref = {textRef}></span></h3>
+      </div>
     </div>
   );
 }
