140d44c79b7b0c6273eacf61f30dcb1970040c2a
java80 <iekedede@hotmail.com>
2021-06-30 14:34:52 -0400

    minor change

--- src/portfolios.js
+++ src/portfolios.js
@@ -7,14 +7,14 @@ export const featuredPortfolio = [
   },
   {
     id: 2,
-    title: "E-commerce App",
+    title: "E-commerce Site",
     img: "https://images.unsplash.com/photo-1426024120108-99cc76989c71?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHdlYnNpdGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     projectLink: "https://ikecart.com/",
   },
 
   {
     id: 3,
-    title: "Dev-it-forward",
+    title: "Dev-it-forward ",
     img: "https://images.unsplash.com/photo-1591439657848-9f4b9ce436b9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTV8fGNvZGluZyUyMHR1dG9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     projectLink: "https://gracious-hodgkin-2b0838.netlify.app/Landing",
   },
