3d86fcc0b6041bb496ff067aa91af10f2e182f0c
java80 <iekedede@hotmail.com>
2021-06-30 13:26:03 -0400

    completed Portfolio component

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import "./Portfolio.scss"
+import { featuredPortfolio } from '../../portfolios';
 const Portfolio = () => {
   return (
-    <div>
-      
+    <div className="portfolio" id="portfolio">
+      <h1>My Portfolio</h1>
+      <div className="container">
+        {featuredPortfolio.map((porfolio) => {
+          return (
+            <a target="blank" href={porfolio.projectLink}>
+              <div className="item">
+                <img src={porfolio.img} alt="" />
+                <h3> {porfolio.title}</h3>
+              </div>
+            </a>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/portfolios.js
@@ -0,0 +1,21 @@
+export const featuredPortfolio = [
+  {
+    id: 1,
+    title: "Course Management App",
+    img: "https://images.unsplash.com/photo-1599658880436-c61792e70672?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fG9ubGluZSUyMGNvdXJzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    projectLink: "https://sad-einstein-72a4bc.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "E-commerce App",
+    img: "https://images.unsplash.com/photo-1426024120108-99cc76989c71?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHdlYnNpdGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    projectLink: "https://ikecart.com/",
+  },
+
+  {
+    id: 3,
+    title: "Dev-it-forward",
+    img: "https://images.unsplash.com/photo-1591439657848-9f4b9ce436b9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTV8fGNvZGluZyUyMHR1dG9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    projectLink: "https://gracious-hodgkin-2b0838.netlify.app/Landing",
+  },
+];
